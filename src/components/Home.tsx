6acde407c72e68fa2172f8c30b643454ff58d333
PedroDev <pedrodev0202@gmail.com>
2024-09-13 14:29:29 -0300

    mudadno tamanho oimg central

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,7 +5,7 @@ export default function Home() {
 return (
     <section id="home" className="rounded-b-md h-[500px] bg-blue-700 flex items-center justify-center text-center">
     <div className="text-white font-montserrat">
-        <img className='w-16 md:w-40 m-auto mb-28' src={img.src} alt="" />
+        <img className='w-28 md:w-40 m-auto mb-28' src={img.src} alt="" />
         <h1 className="text-4xl md:text-6xl font-bold">Bem-vindo ao site das oficinas do CEJAAP</h1>
         <p className="mt-4 text-lg">Você verá aqui, projetos criados nas oficinas.</p>
     </div>
